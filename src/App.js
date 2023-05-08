5a365fab1d503502c6196f2ec812eac264f7535d
welinton <wesgdeveloper@gmail.com>
2023-05-08 14:04:28 -0500

    se modifico componente y se agrego estado a el componente

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
-      </div>
-    );
-  }
+function App() {
+  const [value, setvalue] = useState(0);
+  const handleclick = (e) => {
+    setvalue(value + 1);
+  };
+  return (
+    <div>
+      <span className="value">{value}</span>
+      <button onClick={handleclick} id="inc">
+        Incrementa
+      </button>
+    </div>
+  );
 }
 
 export default App;
